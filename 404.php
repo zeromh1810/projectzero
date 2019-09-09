d28d07b9f006edb8a80fed026d248ec676d53be7
zeromh1810 <c.hickmann86@gmail.com>
2019-09-09 18:21:29 -0300

    cambios css y error 404

--- 404.php
+++ 404.php
@@ -1,6 +1,15 @@
 <?php get_header() ?>
 
-	<h1>Error 404:</h1>
-	<h2>Página no encontrada</h2>
+<section class="container-body">
+        <div class="error-404-wrapper">
+            <div class="logo-hero">
+                <img src="<?php echo get_theme_file_uri() ?>/assets/img/logo-hero.png" alt="">
+            </div>
+            <h2 class="section-title">Error 404 :(</h2>
+            <p>No hemos podido encontrar el sitio que buscas. Vuelve al home e intenta con otra :)</p>
+
+        </div>
+
+    </section>
 
 <?php get_footer() ?>
